a15ab9a6ea08ee535db0d82d424942d4126dde62
Sam Vitello <sam.j.vitello@gmail.com>
2018-04-19 07:07:30 -0700

    get_list_of_top_hodlers

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ These scripts are for analyzing a local node of the blockchain. Note Parity isn'
 ## Scripts
 
 - `find_blocknumber_at_datetime.py` - Find the closest blocknumber for past datetime. This is useful when wanting to find a range of blocks to search through.
+
+- `get_list_of_top_hodlers.py` - Create a list of the top N (default 1,000,000) Ethereum hodlers that were active in a block range.
---
+++ get_list_of_top_hodlers.py
@@ -0,0 +1,81 @@
+import sys
+import csv
+import bisect
+
+NUMBER_OF_HOLDERS = 1000000
+BLOCK_NUMBER = 'eth_blockNumber'
+GET_BALANCE = "eth_getBalance"
+GET_BLOCK = "eth_getBlockByNumber"
+URL = "{}:{}".format("http://localhost", 8545)
+
+class Hodler:
+    def __init__(self, address, balance):
+        self.address = address
+        self.balance = balance
+
+    def __lt__(self, other):
+        return self.balance < other.balance
+
+    def __gt__(self, other):
+        return self.balance > other.balance
+
+    def __eq__(self, other):
+        return self.balance == other.balance
+
+    def as_list(self):
+        return [self.address, self.balance]
+
+
+def rpc_request(method, params = [], key = None):
+    """Make an RPC request to geth on port 8545."""
+    payload = {
+        "method": method,
+        "params": params,
+        "jsonrpc": "2.0",
+        "id": 0
+    }
+
+    res = requests.post(
+          URL,
+          data=json.dumps(payload),
+          headers={"content-type": "application/json"}).json()
+
+    return res['result'][key] if key else res['result']
+
+if __name__ == "__main__":
+    start_block = sys.argv[1]
+    if not start_block:
+        raise RuntimeError("provide a start block")
+
+    seen_addresses = {}
+    sorted_list = list()
+
+    end_block = int(rpc_request(BLOCK_NUMBER, []), 16)
+
+    while (start_block <= end_block):
+        txs = rpc_request(method=GET_BLOCK, params=[start_block, True], key='transactions')
+        for tx in txs:
+            # we consider an address active if it sent or received eth in the last year
+            sender = tx["to"]
+            reciever = tx["from"]
+            # TODO check if contract 'eth_getCode'
+            for addr in [sender, reciever]:
+                if not seen_addresses.get(addr, None):
+                    # We haven't seen this address yet, add to list
+                    balance = int(rpc_request(method=GET_BALANCE, params=[addr]), 16)
+                    seen_addresses[addr] = balance
+                    # if list length is less than limit or value is higher than the lowest element
+                    if balance > 0 and (
+                        len(sorted_list) < NUMBER_OF_HOLDERS or balance > seen_addresses.get(sorted_list[0].balance)
+                    ):
+                        hodler = Hodler(addr, balance)
+                        bisect.insort(sorted_list, hodler)
+
+        start_block += 1
+
+    # We have found all of our addresses and balances (yay!). Time to write to a csv
+    address_csv = open('top_addresses.csv', 'w')
+    address_writer = csv.writer(address_csv, quoting=csv.QUOTE_ALL)
+
+    for hodler in reversed(sorted_list):
+        address_writer.writerow(hodler.as_list())
